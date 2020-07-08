bb957f70489c1ea71df9c5c5b36e66a1e74cbc0e
Sean Olson <srolson@callutheran.edu>
2020-07-08 15:20:43 -0500

    server is storing image upon image upload, front/back end error handling is working

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const Post = require('../models/post');
 const router = express.Router();
+const multer = require('multer');
 
-router.post('', (req, res, next) => {
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg'
+}
+
+// use multer to extract incoming files
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error('Invalid file type');
+    if (isValid) {
+      error = null;
+    } 
+    cb(error, 'backend/images');
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + ext);
+  }
+});
+
+router.post('', multer({storage: storage}).single('image'),(req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -43,10 +43,21 @@ export class PostsService {
       );
   }
 
-  addPost(title: string, content: string) {
-    const post: Post = {id: null, title: title, content: content};
-    this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
+  addPost(title: string, content: string, image: File) {
+    const postData = new FormData();
+    postData.append('title', title);
+    postData.append('content', content);
+    postData.append('image', image, title);
+    this.http
+      .post<{message: string, postId: string}>
+      ('http://localhost:3000/api/posts',
+      postData)
       .subscribe((responseData) => {
+        const post: Post = {
+          id: responseData.postId,
+          title: title,
+          content: content
+        }
         const id = responseData.postId
         post.id = id;
         this.posts.push(post);
