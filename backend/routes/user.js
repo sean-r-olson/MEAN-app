621dd1f48416fdd4dee0590b9b1373401b9c1716
Sean Olson <srolson@callutheran.edu>
2020-07-15 14:40:08 -0500

    added backend logic for user signup authentication and tied to angular frontend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => {
-  // create user and store in db
-  
-})
+  // create new user and store in db
+    // hash/encrypt user's password using bcrypt
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+    user.save()
+      .then(result => {
+        res.status(201).json({
+          message: 'user created',
+          result: result
+        })
+      }).catch(err => {
+        res.status(500).json({
+          error: err
+        })
+      })
+    })
+});
 
 module.exports = router;
